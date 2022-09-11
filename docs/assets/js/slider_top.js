743d8c127f271c2ecdafd90979fd7b8460848295
yuki-n-4122 <yuki.n.4122@gmail.com>
2022-09-11 20:09:03 +0900

    topviewの修正

--- docs/assets/js/slider_top.js
+++ docs/assets/js/slider_top.js
@@ -1,11 +1,10 @@
 var swiper = new Swiper('.swiper-container_top', {
   slidesPerView: 2,
   spaceBetween: 0,
-  effect: 'fade',
   speed: 1000,
   loop: true,
   autoplay: {
-    delay: 3000,
+    delay: 6000,
     disableOnInteraction: false
   },
   pagination: {
