58094bf13087f9239121de0b72250052658d25d2
yuki-n-4122 <yuki.n.4122@gmail.com>
2022-09-11 20:23:13 +0900

    topviewスマホでは1つにする

--- docs/assets/js/slider_top.js
+++ docs/assets/js/slider_top.js
@@ -1,11 +1,16 @@
 var swiper = new Swiper('.swiper-container_top', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 0,
   speed: 1000,
   loop: true,
   autoplay: {
     delay: 6000,
     disableOnInteraction: false
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+    }
   },
   pagination: {
     el: '.swiper-pagination',
