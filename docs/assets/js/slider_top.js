dcbb76e811ba9fda922c8acedf3acb11df651135
yuki-n-4122 <yuki.n.4122@gmail.com>
2022-09-17 18:43:05 +0900

    topスライダーのautoplayを解除

--- docs/assets/js/slider_top.js
+++ docs/assets/js/slider_top.js
@@ -2,10 +2,6 @@ var swiper = new Swiper('.swiper-container_top', {
   effect: 'fade',
   speed: 1000,
   loop: true,
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false
-  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true
